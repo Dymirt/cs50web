d21d469814852a8e7336a098b8e5ed2ca353bc33
Dymirt <kolidadmytro@gmail.com>
2023-07-23 17:29:19 +0200

    adding urls
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- capstone/counters/urls.py
+++ capstone/counters/urls.py
@@ -5,24 +5,48 @@ from django.contrib.auth.decorators import login_required
 
 from .views import (
     CounterListView,
-    ReadingListView,
     CounterDetailView,
     SummaryView,
-    AddCounterReading,
     AddCounter,
     CounterUpdateView,
     ReadingUpdateView,
+    AddReadings,
+    login_view,
+    logout_view,
+    register,
+    index_view,
 )
 
 app_name = "counters"
 
 urlpatterns = [
+    path("index", index_view, name="index"),
+    path("login", login_view, name="login"),
+    path("logout", logout_view, name="logout"),
+    path("register", register, name="register"),
+
+    path("counters/add-readings", AddReadings.as_view(), name="add-readings"),
     # path("counters/", SummaryView.as_view(), name="dashboard"),
     path("counters/summary", SummaryView.as_view(), name="summary"),
     # Counters urls
     path("counters/list/", CounterListView.as_view(), name="counters-list"),
     path("counter/create/", AddCounter.as_view(), name="counter-create"),
     path("counter/<int:pk>/edit/", CounterUpdateView.as_view(), name="counter-edit"),
+
+    path("counter/<int:pk>/detail/", CounterDetailView.as_view(), name="counter-detail"),
+    # Reading urls
+    path("reading/<int:pk>/edit/", ReadingUpdateView.as_view(), name="reading-edit"),
+    path(
+        "reading/<int:pk>/delete/",
+        login_required(
+            DeleteView.as_view(
+                model=Reading,
+                success_url=reverse_lazy("counters:summary"),
+                template_name="counters/generic_delete.html",
+            )
+        ),
+        name="reading-delete",
+    ),
     path(
         "counter/<int:pk>/delete/",
         DeleteView.as_view(
@@ -32,21 +56,5 @@ urlpatterns = [
         )
         ,
         name="counter-delete",
-    ),
-    path("counter/<int:pk>/detail/", CounterDetailView.as_view(), name="counter-detail"),
-    # Reading urls
-    path("readings/list/", ReadingListView.as_view(), name="readings-list"),
-    path("reading/create/", AddCounterReading.as_view(), name="reading-create"),
-    path("reading/<int:pk>/edit/", ReadingUpdateView.as_view(), name="reading-edit"),
-    path(
-        "reading/<int:pk>/delete/",
-        login_required(
-            DeleteView.as_view(
-                model=Reading,
-                success_url=reverse_lazy("counters:readings-list"),
-                template_name="counters/generic_delete.html",
-            )
-        ),
-        name="reading-delete",
     ),
 ]
